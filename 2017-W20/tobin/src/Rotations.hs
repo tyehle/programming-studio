4305ece05153bcd140c00aa43ff5dee9b4b2c13e
Tobin Yehle <tobinyehle@gmail.com>
2017-05-22 16:55:34 -0600

    done

--- 2017-W20/tobin/src/Rotations.hs
+++ 2017-W20/tobin/src/Rotations.hs
@@ -1,4 +1,12 @@
 module Rotations where
 
-rotateWord :: String -> (Integer, String)
-rotateWord word = (0, word)
+import Data.Ord (compare)
+import Data.List (minimumBy)
+
+rotateWord :: String -> (Int, String)
+rotateWord word = minimumBy compareWithIndex allRotations
+  where
+    doubleWord = word ++ word
+    len = length word
+    allRotations = map (\n -> (n, take len (drop n doubleWord))) [0..(len-1)]
+    compareWithIndex (_, a) (_, b) = compare a b
