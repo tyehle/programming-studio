938b48e71388b4b1fe9e5f941b07f1e36673c8a4
gavinyehle <gavin.yehle@gmail.com>
2017-05-29 12:59:28 -0600

    change the whole f***ing thing

--- 2017-W21/gavin.playground/Contents.swift
+++ 2017-W21/gavin.playground/Contents.swift
@@ -3,78 +3,52 @@
 import UIKit
 
 var totalCost = 0
+let testString = "BC BC BC BC BC OH"
+var str: NSString?
+var key: String
 
-var options: Dictionary<String, Int> = [
-    "OH" : 300,
-    "BC" : 110,
-    "SK" : 30
-]
 var ohCount = 0
 var bcCount = 0
 var skCount = 0
 
-func addItem(_ key: String) -> Int {
+let space = CharacterSet(charactersIn: " ")
+var scanner = Scanner(string: testString)
+scanner.charactersToBeSkipped = space
+while scanner.scanUpToCharacters(from: space, into: &str) {
+    key = str! as String
+    print(key, terminator: " ")
     switch key {
     case "OH":
-        totalCost += 300
         ohCount += 1
-    case "BC":
-        totalCost += 110
-        bcCount += 1
+        if ohCount % 3 != 0 {
+            totalCost += 300
+        }
     case "SK":
-        totalCost += 30
         skCount += 1
+        totalCost += 30
+    case "BC":
+        bcCount += 1
+        totalCost += 110
     default:
         break
     }
-    print(totalCost)
-    return totalCost
+    print("t \(totalCost)")
 }
 
-func skyTourDiscount() -> Int {
-    if ohCount != 0 && skCount != 0 {
-        totalCost -= 30
-        skCount -= 1
-        print("sky tour discount \(totalCost+30) - 30 = \(totalCost)")
-        skyTourDiscount()
-    }
-    return totalCost
+var test: Bool = (skCount != 0) && (ohCount != 0)
+while test {
+    totalCost -= 30
+    skCount -= 1
+    ohCount -= 1
+    test = (skCount != 0) && (ohCount != 0)
 }
+print(totalCost)
 
-func operaHouseDiscount() -> Int {
-    if ohCount >= 3 {
-        totalCost -= 300
-        ohCount -= 3
-        print("opera house discount \(totalCost+300) - 300 = \(totalCost)")
-        operaHouseDiscount()
-    }
-    return totalCost
+
+if bcCount > 4 {
+    totalCost -= 20*bcCount
 }
-
-func bridgeClimbDiscount() -> Int {
-    if bcCount >= 4 {
-        let discount = 20 * bcCount
-        totalCost -= discount
-        bcCount -= 4
-        print("bridge climb discount \(totalCost+discount) - \(discount) = \(totalCost)")
-        bridgeClimbDiscount()
-    }
-    return totalCost
-}
+print(totalCost)
 
 
 
-
-
-
-addItem("OH")
-addItem("OH")
-addItem("OH")
-addItem("BC")
-addItem("BC")
-addItem("BC")
-addItem("BC")
-addItem("BC")
-skyTourDiscount()
-operaHouseDiscount()
-bridgeClimbDiscount()
