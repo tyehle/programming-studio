fee86b7ce91d1fe984034c1833c4cffdfc5af179
Tobin Yehle <tobinyehle@gmail.com>
2017-07-01 16:32:22 -0600

    get links outside of parens

--- 2017-W25/tobin/app/Main.hs
+++ 2017-W25/tobin/app/Main.hs
@@ -1,22 +1,8 @@
-{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Data.List (isPrefixOf)
-import Text.HTML.Scalpel
-
-import Scraper
+import Scraper (getLinks)
 
 main :: IO ()
 main = do
-  result <- getLinks "https://en.wikipedia.org/wiki/science"
-  maybe (putStrLn "Error") (mapM_ print . take 20) result
-
-
-getLinks :: URL -> IO (Maybe [String])
-getLinks url = scrapeURL url scraper
-  where
-    internal = isPrefixOf "/wiki/"
-    selectBody = "div" @: ["id" @= "mw-content-text"]
-    scraper = do
-      allLinks <- attrs "href" $ selectBody // "p" // "a"
-      return . filter internal $ allLinks
+  maybeLinks <- getLinks "https://en.wikipedia.org/wiki/science"
+  maybe (putStrLn "Error") (mapM_ print . take 20) maybeLinks
--- 2017-W25/tobin/src/Scraper.hs
+++ 2017-W25/tobin/src/Scraper.hs
@@ -1,6 +1,36 @@
-module Scraper
-    ( someFunc
-    ) where
+{-# LANGUAGE OverloadedStrings #-}
+module Scraper where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.List (isPrefixOf)
+import Control.Monad (void)
+import Text.HTML.Scalpel
+import Text.Parsec
+import Text.Parsec.Char
+
+getLinks :: URL -> IO (Maybe [String])
+getLinks url = do
+  rawText <- scrapeURL url getPText
+  let noParens = rawText >>= dropParens
+  let links = noParens >>= flip scrapeStringLike linksFromText
+  return links
+
+
+linksFromText :: Scraper String [String]
+linksFromText = filter internal <$> attrs "href" "a"
+  where
+    internal = isPrefixOf "/wiki/"
+
+getPText :: Scraper String String
+getPText = do
+  inners <- innerHTMLs $ "div" @: ["id" @= "mw-content-text"] // "p"
+  return . concat $ inners
+
+
+dropParens :: String -> Maybe String
+dropParens = either (const Nothing) Just . parse (parser <* eof) "function input"
+  where
+    parser = do
+      before <- many $ noneOf "()"
+      after <- (parenParser >> parser) <|> return ""
+      return $ before ++ after
+    parenParser = void $ between (char '(') (char ')') parser
