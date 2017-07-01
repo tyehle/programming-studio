790108e7a539546d475f7c1c8f36b94001e3b53c
Tobin Yehle <tobinyehle@gmail.com>
2017-07-01 14:35:41 -0600

    scrape all internal links

--- 2017-W25/tobin/app/Main.hs
+++ 2017-W25/tobin/app/Main.hs
@@ -1,6 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Lib
+import Data.List (isPrefixOf)
+import Text.HTML.Scalpel
+
+import Scraper
 
 main :: IO ()
-main = someFunc
+main = do
+  result <- getLinks "https://en.wikipedia.org/wiki/science"
+  maybe (putStrLn "Error") (mapM_ print . take 20) result
+
+
+getLinks :: URL -> IO (Maybe [String])
+getLinks url = scrapeURL url scraper
+  where
+    internal = isPrefixOf "/wiki/"
+    selectBody = "div" @: ["id" @= "mw-content-text"]
+    scraper = do
+      allLinks <- attrs "href" $ selectBody // "p" // "a"
+      return . filter internal $ allLinks
