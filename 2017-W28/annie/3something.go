9fdc7846a1c2719df543b19eeb8732ebb70587ef
Annie Cherkaev <annie.cherk@gmail.com>
2017-07-22 15:48:17 -0600

    bugfix

--- 2017-W28/annie/3something.go
+++ 2017-W28/annie/3something.go
@@ -5,31 +5,34 @@ import (
   "sort"
 )
 
+/*
+Only possibly a direct implementation of the wiki pseudocode:
+https://en.wikipedia.org/wiki/3SUM
+*/
 
 func menageatrois(arr []int) {
-   sort.Ints( arr )
-   for outer := 0; outer < len(arr); outer++ {
+  sort.Ints( arr )
 
-  var a = arr[outer]
-        var start = outer+1
-  var end = len(arr)-1
+  for outer := 0; outer < len(arr); outer++ {
+    var a = arr[outer]
+    var start = outer+1
+    var end = len(arr)-1
 
-  for ; start < end ; {
-    var b = arr[start]
-    var c = arr[end]
+    for ; start < end ; {
+      var b = arr[start]
+      var c = arr[end]
 
-    switch {
-    case a+b+c == 0:
-          fmt.Println(a, b, c)
-      end -=1
-    case a+b+c > 0:
-      end -=1
-    default:
-      start += 1
+      switch {
+      case a+b+c == 0:
+        fmt.Println(a, b, c)
+        end -=1
+      case a+b+c > 0:
+        end -=1
+      default:
+        start += 1
+      }
     }
-      end -= 1
   }
- }
 }
 
 
@@ -43,8 +46,16 @@ func menageatrois(arr []int) {
 */
 
 func main() {
-    arr := []int {9, -6, -5, 9, 8, 3, -4, 8, 1, 7, -4, 9, -9, 1, 9, -9, 9, 4, -6, -8}
-    menageatrois( arr )
+    tests := [][]int{{9, -6, -5, 9, 8, 3, -4, 8, 1, 7, -4, 9, -9, 1, 9, -9, 9, 4, -6, -8},
+                    {4, 5, -1, -2, -7, 2, -5, -3, -7, -3, 1},
+                    {-1, -6, -3, -7, 5, -8, 2, -8, 1},
+                    {-5, -1, -4, 2, 9, -9, -6, -1, -7}}
+
+
+    for i := 0; i < len(tests); i++ {
+      fmt.Println("   Running dataset ", i, ": ", tests[i])
+      menageatrois( tests[i] )
+      fmt.Println("")
+    }
     fmt.Println("Done!")
 }
-
